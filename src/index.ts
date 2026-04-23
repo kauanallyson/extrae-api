be6500f92345f82d87d226cb0bf3b33257c0ff1f
kauanallyson <163926307+kauanallyson@users.noreply.github.com>
2026-04-23 13:45:52 -0300

    fix: update hostname on server

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ const app = new Elysia()
 	.use(laudoRoutes)
 	.use(profissionaisRoutes)
 	.use(excelRoutes)
-	.listen(3000);
+	.listen({ hostname: "0.0.0.0", port: 3000 });
 
 console.log(
 	`🦊 Elysia is running at http://${app.server?.hostname}:${app.server?.port}`,
